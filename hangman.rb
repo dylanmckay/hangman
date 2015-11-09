92f1e4fe06a42a4e7e2eba35c33e736a8fa2a197
Dylan McKay <dylanmckay@Dylans-MacBook-Pro.local>
2015-11-10 11:09:09 +1300

    Refactored to use the MVC pattern

--- hangman.rb
+++ hangman.rb
@@ -2,93 +2,134 @@
 
 GUESS_COUNT = 8
 
-class Hangman
-  attr_reader :word
+class HangmanModel
+  attr_reader :word, :remaining_turns
 
   def initialize(word)
     @word = word
     @correct_letters = []
-  end
-
-  def perform_turn(letter)
-    if @word.chars.include?(letter) 
-      @correct_letters << letter
-    end
+    @remaining_turns = GUESS_COUNT
   end
 
   def word_guessed?
     @correct_letters.length == @word.chars.uniq.length
   end
 
-  def current_word_status
-   @word.chars.map do |c|
-      if @correct_letters.include?(c)
-        c
-      else
-        '_'
-      end
-   end.join
+  def contain_letter?(letter)
+    @word.chars.include?(letter)
+  end
+
+  def give_correct_letter(letter)
+    fail if !contain_letter?(letter)
+    @correct_letters<< letter
   end
 
   def already_guessed?(letter)
     @correct_letters.include?(letter)
   end
+
+  def remaining_turns?
+    @remaining_turns > 0
+  end
+
+  def decrement_remaining_turns
+    @remaining_turns -= 1
+  end
+
+  # gets a the word as a character array, with unguessed letters
+  # being represented by 'nil'.
+  def status_chars
+    @word.chars.map { |c| @correct_letters.include?(c) ? c : nil }
+  end
 end
 
-class UI
-  def initialize(game)
-    @game = game
+class HangmanController
+  def initialize(model, view)
+    @model = model
+    @view = view
   end
 
   def play
+    @view.show_guess_count(@model.remaining_turns)
 
-  puts "You have #{GUESS_COUNT} guesses"
+    while @model.remaining_turns?
+      play_turn
 
-  GUESS_COUNT.times do
-    play_turn
+      if @model.word_guessed?
+        @view.show_win_message(@model.word)
+        return
+      end
 
-    if @game.word_guessed?
-      puts("You won! The word was #{@game.word}")
-      return
+      @model.decrement_remaining_turns
     end
-  end
 
-  puts("You ran out of turns :( The word was '#{@game.word}'")
+    @view.show_lose_message(@model.word)
   end
 
   def play_turn
-  puts("The word is '#{@game.current_word_status}'")
+    @view.show_word_status(@model.status_chars)
 
-  letter = prompt_letter
+    letter = @view.prompt_letter
 
-  if @game.already_guessed?(letter)
-    puts("You have already correctly guessed this")
-  else
-    @game.perform_turn(letter)
-  end
-  end
-
-  def prompt_letter
-  letter = nil
-
-  # wait for valid input
-  until letter
-    print("Enter a letter: ")
-    letter = gets.chomp
-
-    if letter.length != 1
-      puts("Please enter a single character")
-      letter = nil
+    if @model.already_guessed?(letter)
+      @view.show_already_guessed_message(letter)
+    else
+      process_letter(letter)
     end
   end
-  letter
+
+  def process_letter(letter)
+    if @model.contain_letter?(letter)
+      @model.give_correct_letter(letter)
+    end
+  end
+end
+
+class HangmanView
+  def prompt_letter
+    letter = nil
+
+    # wait for valid input
+    until letter
+      print("Enter a letter: ")
+      letter = gets.chomp
+
+      if letter.length != 1
+        puts("Please enter a single character")
+        letter = nil
+      end
+    end
+    letter
   end
 
+  def show_guess_count(count)
+    puts "You have #{count} guesses"
+  end
 
+  def show_win_message(word)
+      puts("You won! The word was '#{word}'")
+  end
+
+  def show_lose_message(word)
+    puts("You ran out of turns :( The word was '#{word}'")
+  end
+
+  def show_word_status(chars)
+    puts("The word is '#{current_word_status_str(chars)}'")
+  end
+
+  def show_already_guessed_message(letter)
+      puts("You have already correctly guessed '#{letter}'")
+  end
+
+  def current_word_status_str(chars)
+    chars.map { |c| c == nil ? '_' : c }.join
+  end
 end
 
 word = File.read("words.txt").split("\n").sample()
-game = Hangman.new(word)
-ui = UI.new(game)
+model = HangmanModel.new(word)
+view = HangmanView.new()
+controller = HangmanController.new(model, view)
 
-ui.play
+controller.play
